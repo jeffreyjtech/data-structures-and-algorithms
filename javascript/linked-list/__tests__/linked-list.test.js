fdeb8f06e9911ceed62cc7697b24de4db3ceb796
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-25 18:49:48 -0700

    Compeleted test for creating empty list

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,55 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
+  it('Can successfully instantiate an empty linked list', () => {
+    let newLL = new LinkedList;
+
+    expect(newLL.head).toBeNull();
+  });
+
+  it('Can properly insert into the linked list', () => {
+    let newLL = new LinkedList;
+
+    let valueA = 12;
+
+    newLL.insert(valueA);
+
+    expect(newLL.head.value).toEqual(valueA);
+  });
+
+  it('The head property will properly point to the first node in the linked list', () => {
+    let newLL = new LinkedList;
+
+    
+  });
+
+  it('Can properly insert multiple nodes into the linked list', () => {
+    let newLL = new LinkedList;
+
+    let valueA = 12;
+    let valueB = -13;
+
+    newLL.insert(valueA);
+    newLL.insert(valueB);
+
+    console.log(newLL);
+
+    expect(newLL.head.value).toEqual(valueB);
+    expect(newLL.head.next.value).toEqual(valueA);
+  });
+
+  it('Will return true when finding a value within the linked list that exists', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('Will return false when searching for a value in the linked list that does not exist', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('Can properly return a collection of all the values that exist in the linked list', () => {
     expect(true).toBeTruthy();
   });
 });
