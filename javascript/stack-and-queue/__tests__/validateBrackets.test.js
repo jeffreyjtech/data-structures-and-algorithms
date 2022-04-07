c8351d9babd6bac93dfcb7413b88d6955df40f4b
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-07 00:25:48 -0700

    Added more tests

--- javascript/stack-and-queue/__tests__/validateBrackets.test.js
+++ javascript/stack-and-queue/__tests__/validateBrackets.test.js
@@ -9,4 +9,24 @@ describe('Testing validateBrackets function', () => {
     expect(validateBrackets('[]')).toBe(true);
     expect(validateBrackets('()')).toBe(true);
   });
+
+  test('Passes nested brackets of different types', () => {
+    expect(validateBrackets('([{}])')).toBe(true);
+  });
+
+  test('Passes text with each type of bracket', () => {
+    expect(validateBrackets('(das)[{adsghlaskdj}asfd1243]')).toBe(true);
+  });
+
+  test('Fails string with no brackets', () => {
+    expect(validateBrackets('no brackets?')).toBe(false);
+  });
+
+  test('Fails string with only one bracket', () => {
+    expect(validateBrackets('one bracket!)')).toBe(false);
+  });
+  
+  test('Fails string with only two brackets of different types', () => {
+    expect(validateBrackets('(}')).toBe(false);
+  });
 });
