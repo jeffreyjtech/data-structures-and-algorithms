d45090ec1f446b67eb9683fdcd059c26410d1ce6
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-01 23:02:05 -0700

    Finished hashtable first implementation tests

--- javascript/hashtable/hashtable.test.js
+++ javascript/hashtable/hashtable.test.js
@@ -29,9 +29,26 @@ describe('Testing HashTable implementation', () => {
     expect(keys.includes('Hello')).toBe(true);
   });
 
-  // test('Successfully handle a collision within the hashtable', () => {
-  //   let keys = testTable.keys();
+  test('Successfully handle a collision within the hashtable', () => {
+    expect(testTable.hash('Hdmlo')).toBe(testTable.hash('Hello'));
+    testTable.set('Hdmlo', 'bob');
+    expect(testTable.buckets).toEqual(expect.arrayContaining([expect.anything()]));
+  });
 
-  //   expect(keys.includes('Hello')).toBe(true);
-  // });
+  test('Successfully retrieve a value from a bucket within the hashtable that has a collision', () => {
+    let value = testTable.get('Hdmlo');
+
+    expect(value).toBe('bob');
+    value = testTable.get('Hello');
+
+    expect(value).toBe('World');
+  });
+
+  test('Successfully hash a key to an in-range value', () => {
+    testTable.set('testy key', 'testy value');
+    for (let i = 0; i < 1000; i++){
+      testTable.set((Math.random().toString()), Math.random());
+    }
+    expect(testTable.buckets.length).toBe(1024);
+  });
 });
