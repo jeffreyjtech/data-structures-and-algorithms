5b60df153ef6218a27666fc30cac0811053219b2
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-01 21:19:15 -0700

    Changed tests to reflect that quickSort sorts in-place

--- javascript/quick-sort/quick-sort.test.js
+++ javascript/quick-sort/quick-sort.test.js
@@ -6,43 +6,43 @@ describe('Testing quickSort function', () => {
 
   test('Can sort a 2-number array', () => {
     let testArr = [34, 6];
-    let result = quickSort(testArr);
+    quickSort(testArr);
 
-    expect(result).toEqual([6, 34]);
+    expect(testArr).toEqual([6, 34]);
   });
 
   test('Can sort a 3-number array', () => {
     let testArr = [34, 6, 12];
-    let result = quickSort(testArr);
+    quickSort(testArr);
 
-    expect(result).toEqual([6, 12, 34]);
+    expect(testArr).toEqual([6, 12, 34]);
   });
 
   test('Can sort a 6-number array', () => {
     let testArr = [34, 6, 12, 100, -2, 40];
-    let result = quickSort(testArr);
+    quickSort(testArr);
 
-    expect(result).toEqual([-2, 6, 12, 34, 40, 100]);
+    expect(testArr).toEqual([-2, 6, 12, 34, 40, 100]);
   });
 
   test('Does not change an already-sorted array', () => {
     let testArr = [101, 102, 103];
-    let result = quickSort(testArr);
+    quickSort(testArr);
 
-    expect(result).toEqual([101, 102, 103]);
+    expect(testArr).toEqual([101, 102, 103]);
   });
 
   test('Can sort a reverse-sorted array', () => {
     let testArr = [25, 10, 0, -20];
-    let result = quickSort(testArr);
+    quickSort(testArr);
 
-    expect(result).toEqual([-20, 0, 10, 25]);
+    expect(testArr).toEqual([-20, 0, 10, 25]);
   });
 
   test('Can sort an array with duplicate values', () => {
     let testArr = [10, 10, 0, 0, 0, -20];
-    let result = quickSort(testArr);
+    quickSort(testArr);
 
-    expect(result).toEqual([-20, 0, 0, 0, 10, 10]);
+    expect(testArr).toEqual([-20, 0, 0, 0, 10, 10]);
   });
 });
