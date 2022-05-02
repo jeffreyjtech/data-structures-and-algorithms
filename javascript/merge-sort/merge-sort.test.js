196a03708ed88e8937ce3b8ced8e3a5be87c3a2a
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-01 21:05:38 -0700

    Fixed naming in merge-sort test

--- javascript/merge-sort/merge-sort.test.js
+++ javascript/merge-sort/merge-sort.test.js
@@ -1,47 +1,47 @@
 'use strict';
 
-const insertionSort = require('./');
+const mergeSort = require('./');
 
-describe('Testing insertionSort function', () => {
+describe('Testing mergeSort function', () => {
 
   test('Can sort a 2-number array', () => {
     let testArr = [34, 6];
-    let result = insertionSort(testArr);
+    let result = mergeSort(testArr);
 
     expect(result).toEqual([6, 34]);
   });
 
   test('Can sort a 3-number array', () => {
     let testArr = [34, 6, 12];
-    let result = insertionSort(testArr);
+    let result = mergeSort(testArr);
 
     expect(result).toEqual([6, 12, 34]);
   });
 
   test('Can sort a 6-number array', () => {
     let testArr = [34, 6, 12, 100, -2, 40];
-    let result = insertionSort(testArr);
+    let result = mergeSort(testArr);
 
     expect(result).toEqual([-2, 6, 12, 34, 40, 100]);
   });
 
   test('Does not change an already-sorted array', () => {
     let testArr = [101, 102, 103];
-    let result = insertionSort(testArr);
+    let result = mergeSort(testArr);
 
     expect(result).toEqual([101, 102, 103]);
   });
 
   test('Can sort a reverse-sorted array', () => {
     let testArr = [25, 10, 0, -20];
-    let result = insertionSort(testArr);
+    let result = mergeSort(testArr);
 
     expect(result).toEqual([-20, 0, 10, 25]);
   });
 
   test('Can sort an array with duplicate values', () => {
     let testArr = [10, 10, 0, 0, 0, -20];
-    let result = insertionSort(testArr);
+    let result = mergeSort(testArr);
 
     expect(result).toEqual([-20, 0, 0, 0, 10, 10]);
   });
