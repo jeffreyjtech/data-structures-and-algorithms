80d07bd0b64b03c3353fb6f7c490e12b4bad0d05
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 18:27:32 -0700

    Created treeIntersection function & tests

--- javascript/treeIntersection/tree-intersection.js
+++ javascript/treeIntersection/tree-intersection.js
@@ -1,3 +1,22 @@
 'use strict';
 
+const Hashtable = require('../hashtable/Hashtable');
 
+function treeIntersection(treeA, treeB) {
+  let valuesTable = new Hashtable(1024);
+  let duplicates = [];
+  const checkValues = (value) => {
+    const duplicateFlag = valuesTable.setOrGet(value.toString(), value);
+    console.log(valuesTable);
+    console.log(duplicateFlag);
+    if (duplicateFlag) {
+      duplicates.push(value);
+    }
+  };
+  treeA.traverseWithCallback(checkValues);
+  treeB.traverseWithCallback(checkValues);
+
+  return duplicates;
+}
+
+module.exports = treeIntersection;
