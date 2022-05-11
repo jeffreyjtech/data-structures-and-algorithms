752bc81d670c7a719dee941dfa9f2690d009109e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-11 02:01:49 -0700

    Completed businessTrip function & tests

--- javascript/graphs/business-trip.js
+++ javascript/graphs/business-trip.js
@@ -1,2 +1,21 @@
 'use strict';
 
+function businessTrip(graph, locations) {
+  let current = locations.shift();
+  let totalCost = 0;
+  while (locations.length > 0) {
+    const neighbors = graph.getNeighbors(current);
+    current = locations.shift();
+    for (let i = 0; i < neighbors.length; i++) {
+      if (neighbors[i].vertex === current) {
+        totalCost += neighbors[i].weight;
+        break;
+      } else if (i+1 === neighbors.length) {
+        return [false, 0];
+      }
+    }
+  }
+  return [true, totalCost];
+}
+
+module.exports = businessTrip;
